d76d94f794c92db63e701fe57d3369fea76550f4
nhandq-dev <56851883+nhandq-dev@users.noreply.github.com>
2023-10-25 09:57:44 +0700

    Time: 388 ms (9.7%), Space: 44.8 MB (55.18%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
@@ -0,0 +1,43 @@
+function lengthOfLIS(nums: number[]): number {
+    const dp: number[] = [];
+    let res = 1;
+
+    for (let i=0; i<nums.length; i++) {
+        for (let j=0; j<i; j++) {
+            if (nums[i] > nums[j] && (dp[i] === undefined || dp[i] < dp[j] + 1)) {
+                dp[i] = (dp[j] || 1) + 1
+                res = Math.max(res, dp[i])
+            }
+        }
+    }
+
+    return res
+};
+
+// function lengthOfLIS(nums: number[]): number {
+//     const memos: Map<string, number> = new Map();
+
+//     const dp = (idx: number, lastIdx: number = undefined): number => {
+//         if (nums[idx] === undefined) return 0;
+
+//         if (!memos.has(`${idx}-${lastIdx}`)) {
+//             if (!nums[lastIdx]) {
+//                 memos.set(`${idx}-${lastIdx}`, Math.max(
+//                     1 + dp(idx+1, idx),
+//                     dp(idx+1, lastIdx)
+//                 ))
+//             } else if (nums[idx] > nums[lastIdx]) {
+//                 memos.set(`${idx}-${lastIdx}`, nums[idx] - nums[lastIdx] === 1 ? 1 + dp(idx+1, idx) : Math.max(
+//                     1 + dp(idx+1, idx),
+//                     dp(idx+1, lastIdx)
+//                 ))
+//             } else {
+//                 memos.set(`${idx}-${lastIdx}`, dp(idx+1, lastIdx))
+//             }
+//         }
+
+//         return memos.get(`${idx}-${lastIdx}`);
+//     }
+
+//     return dp(0)
+// };
