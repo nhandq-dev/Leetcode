b19824d813afdc92f3c7fdbf1f54035d7f01ebb0
nhandq-dev <56851883+nhandq-dev@users.noreply.github.com>
2023-10-25 10:01:07 +0700

    Time: 122 ms (63.21%), Space: 44.7 MB (55.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
@@ -3,9 +3,10 @@ function lengthOfLIS(nums: number[]): number {
     let res = 1;
 
     for (let i=0; i<nums.length; i++) {
+        dp[i] = 1;
         for (let j=0; j<i; j++) {
-            if (nums[i] > nums[j] && (dp[i] === undefined || dp[i] < dp[j] + 1)) {
-                dp[i] = (dp[j] || 1) + 1
+            if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1
                 res = Math.max(res, dp[i])
             }
         }
