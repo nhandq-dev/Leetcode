a4384ef2406d8ee731d21f2207a44d9b44a29bcc
nhandq-dev <56851883+nhandq-dev@users.noreply.github.com>
2023-12-12 13:44:08 +0700

    Time: 71 ms (34.16%), Space: 48 MB (10.45%) - LeetHub

--- 0046-permutations/0046-permutations.ts
+++ 0046-permutations/0046-permutations.ts
@@ -1,15 +1,13 @@
 function permute(nums: number[]): number[][] {
-    switch (nums.length) {
-        case 1:
-            return [nums]
-        default:
-            let res: number[][] = [];
+    if (nums.length === 1) return [nums]
 
-            for (let i: number = 0; i < nums.length; i++) {
-                for (const subArr of permute([...nums.slice(0, i), ...nums.slice(i + 1)])) {
-                    res.push([nums[i], ...subArr]);
-                }
-            }
-            return res;
+    let res: number[][] = [];
+
+    for (let i: number = 0; i < nums.length; i++) {
+        for (const subArr of permute([...nums.slice(0, i), ...nums.slice(i + 1)])) {
+            res.push([nums[i], ...subArr]);
+        }
     }
+
+    return res;
 };
