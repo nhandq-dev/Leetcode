e33808482a103b11789237fdd2b77b295dc9915f
nhandq-dev <56851883+nhandq-dev@users.noreply.github.com>
2023-12-13 13:18:43 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -0,0 +1,16 @@
+function subsetsWithDup(nums: number[]): number[][] {
+    const res: Map<string, number[]> = new Map()
+
+    const traveling = (idx: number, prefix: number[]) => {
+        if (nums[idx] === undefined) {
+            res.set(JSON.stringify(prefix), prefix)
+            return
+        }
+
+        traveling(idx+1, prefix)
+        traveling(idx+1, [...prefix, nums[idx]])
+    }
+    traveling(0, [])
+
+    return Array.from(res.values());
+};
