c7563930b4163816d64f2405e285c9b41be2b871
nhandq-dev <56851883+nhandq-dev@users.noreply.github.com>
2023-12-14 10:27:32 +0700

    Time: 369 ms (35.71%), Space: 149.3 MB (21.43%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.ts
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.ts
@@ -1,30 +1,38 @@
 function onesMinusZeros(grid: number[][]): number[][] {
-    const numRows = grid.length;
-    const numCols = grid[0].length;
+    const oneRow: number[] = []
+    const zeroRow: number[] = []
+    const oneCol: number[] = []
+    const zeroCol: number[] = []
+    const res: number[][] = []
 
-    const oneRow: number[] = Array(numRows).fill(0);
-    const zeroRow: number[] = Array(numRows).fill(0);
-    const oneCol: number[] = Array(numCols).fill(0);
-    const zeroCol: number[] = Array(numCols).fill(0);
-    const res: number[][] = Array(numRows).fill(null).map(() => Array(numCols).fill(0));
+    for (let i = 0; i < grid.length; i++) {
+        [oneRow[i], zeroRow[i]] = grid[i].reduce((carry: number[], item: number) => {
+            return item === 0 ? [
+                carry[0],
+                1 + carry[1]
+            ] : [
+                1 + carry[0],
+                carry[1]
+            ]
+        }, [0, 0])
+        res[i] = []
 
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < numCols; j++) {
-            if (j === 0) {
-                [oneRow[i], zeroRow[i]] = grid[i].reduce((carry: number[], item: number) => {
-                    return item === 0 ? [carry[0], 1 + carry[1]] : [1 + carry[0], carry[1]];
-                }, [0, 0]);
-            }
-
-            if (i === 0) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (oneCol[j] === undefined || zeroCol[j] === undefined) {
                 [oneCol[j], zeroCol[j]] = grid.reduce((carry: number[], item: number[]) => {
-                    return item[j] === 0 ? [carry[0], 1 + carry[1]] : [1 + carry[0], carry[1]];
-                }, [0, 0]);
+                    return item[j] === 0 ? [
+                        carry[0],
+                        1 + carry[1]
+                    ] : [
+                        1 + carry[0],
+                        carry[1]
+                    ]
+                }, [0, 0])
             }
 
-            res[i][j] = oneRow[i] + oneCol[j] - zeroRow[i] - zeroCol[j];
+            res[i][j] = oneRow[i] + oneCol[j] - zeroRow[i] - zeroCol[j]
         }
     }
 
-    return res;
-}
+    return res
+};
