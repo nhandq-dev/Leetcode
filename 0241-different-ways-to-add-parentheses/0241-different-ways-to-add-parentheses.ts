1f78e667be6d1aa35213d8a154bb095541ab8fb2
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2023-12-23 13:50:48 +0700

    Time: 66 ms (46.67%), Space: 45.6 MB (33.33%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.ts
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.ts
@@ -1,35 +1,28 @@
 function diffWaysToCompute(expression: string): number[] {
+    const calculate = (num1: number, num2: number, operator: string): number => {
+        switch (operator) {
+            case '+':
+                return num1 + num2
+            case '-':
+                return num1 - num2
+            case '*':
+                return num1 * num2
+        }
+    }
+
     const dp = (expression: string): number[] => {
         const res: number[] = []
-        let idx = 0
-        while (expression[idx] !== undefined) {
-            if (expression[idx] === '+') {
-                const firstPaths = dp(expression.slice(0, idx))
-                const secondPaths = dp(expression.slice(idx + 1))
+        for (let i = 0; i < expression.length; i++) {
+            if (isNaN(parseInt(expression[i]))) {
+                const firstPaths = dp(expression.slice(0, i))
+                const secondPaths = dp(expression.slice(i + 1))
+
                 for (const firstPath of firstPaths) {
                     for (const secondPath of secondPaths) {
-                        res.push(firstPath + secondPath)
-                    }
-                }
-            } else if (expression[idx] === '-') {
-                const firstPaths = dp(expression.slice(0, idx))
-                const secondPaths = dp(expression.slice(idx + 1))
-                for (const firstPath of firstPaths) {
-                    for (const secondPath of secondPaths) {
-                        res.push(firstPath - secondPath)
-                    }
-                }
-            } else if (expression[idx] === '*') {
-                const firstPaths = dp(expression.slice(0, idx))
-                const secondPaths = dp(expression.slice(idx + 1))
-                for (const firstPath of firstPaths) {
-                    for (const secondPath of secondPaths) {
-                        res.push(firstPath * secondPath)
+                        res.push(calculate(firstPath, secondPath, expression[i]))
                     }
                 }
             }
-
-            idx++
         }
 
         return res.length === 0 ? [parseInt(expression)] : res
