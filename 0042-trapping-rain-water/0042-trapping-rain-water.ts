81fecabbf54fbad3914ccf74b322e44c7a508386
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-01-17 22:35:30 +0700

    Time: 84 ms (33.45%), Space: 45.2 MB (79.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.ts
+++ 0042-trapping-rain-water/0042-trapping-rain-water.ts
@@ -1,22 +1,28 @@
 function trap(height: number[]): number {
     let res = 0
     const n = height.length
+    let leftWindow = height[0]
+    let rightWindow = []
+
+    for (let i = 1; i < n; i++) {
+        while (rightWindow[rightWindow.length-1] < height[i] && rightWindow.length !== 0) {
+            rightWindow.pop()
+        }
+
+        rightWindow.push(height[i])
+    }
 
     for (let i = 1; i < n - 1; i++) {
-        const biggestLeftBuilding = Math.max(...height.slice(0, i+1))
-        const biggestRightBuilding = Math.max(...height.slice(i))
+        leftWindow = Math.max(leftWindow, height[i])
+        const biggestLeftBuilding = leftWindow
+
+        const biggestRightBuilding = rightWindow[0];
+        if (height[i] === rightWindow[0]) {
+            rightWindow.shift()
+        }
 
         res += Math.min(biggestLeftBuilding, biggestRightBuilding) - height[i]
     }
 
     return res
 };
-
-/**
-*
-* *
-***
-***
-
-
- */
