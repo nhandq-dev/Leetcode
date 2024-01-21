dd1ffcfa2d094e7796306f0327209f9f2f7c9b6c
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-01-21 11:29:34 +0700

    Time: 304 ms (6%), Space: 60.7 MB (6%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.ts
+++ 0402-remove-k-digits/0402-remove-k-digits.ts
@@ -1,38 +1,21 @@
 function removeKdigits(num: string, k: number): string {
-    // let res = Number.POSITIVE_INFINITY
-
-    // const dp = (str: string, removingCount: number) => {
-    //     if (removingCount === 0) {
-    //         res = isNaN(parseInt(str)) ? 0 : Math.min(res, parseInt(str))
-    //     }
-
-    //     for (let i=0; i< str.length; i++) {
-    //         dp(`${str.slice(0, i)}${str.slice(i+1)}`, removingCount-1)
-    //     }
-    // }
-    // dp(num, k)
-
-    // return res.toString()
-
-
-    // increcing monolothic stack
     const stack: number[] = []
-    for (let i=0; i<num.length; i++) {
+    for (let i = 0; i < num.length; i++) {
         const number = parseInt(num[i])
 
-        while (stack.length !== 0 && parseInt(num[stack[stack.length-1]]) > number) {
+        while (stack.length !== 0 && parseInt(num[stack[stack.length - 1]]) > number && k !== 0) {
             const candidateIdx = stack.pop()
 
-            num = num.slice(0, candidateIdx) + num.slice(candidateIdx+1)
+            num = num.slice(0, candidateIdx) + num.slice(candidateIdx + 1)
             i--
             k--
-            if (k === 0) break
         }
         if (k === 0) break
 
 
         stack.push(i)
     }
+
     while (k !== 0) {
         num = num.slice(0, -1)
         k--
@@ -44,18 +27,3 @@ function removeKdigits(num: string, k: number): string {
 
     return num === '' ? '0' : num
 };
-
-/**
-123456789
-
-
-1432219 k=3
-
-132219 k=2 -remove 4
-
-12349 k=1 -remove 3
-
-
-
-
- */
