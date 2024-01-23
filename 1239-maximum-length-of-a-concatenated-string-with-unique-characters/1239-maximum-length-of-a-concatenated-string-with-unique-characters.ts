ba373ddf223ac73f2d5ccf9385c23520674dd1a4
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-01-23 14:41:41 +0700

    Time: 87 ms (79.17%), Space: 62 MB (12.5%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.ts
@@ -0,0 +1,45 @@
+function maxLength(arr: string[]): number {
+    const encodeStr = (str: string): number => {
+        let binaryStr: string = '00000000000000000000000000'
+
+        for (let i = 0; i < str.length; i++) {
+            const currChar = str.charCodeAt(i) - 97; // 97 was the code for the first letter a
+            if (binaryStr[currChar] === '1') return 0
+
+            binaryStr = binaryStr.slice(0, currChar) + '1' + binaryStr.slice(currChar + 1)
+        }
+
+        return parseInt(binaryStr, 2)
+    }
+
+    const encodedArr: number[] = []
+    const filteredArr: string[] = []
+
+    for (const item of arr) {
+        const encodedItem = encodeStr(item)
+        if (encodedItem !== 0) {
+            encodedArr.push(encodedItem)
+            filteredArr.push(item)
+        }
+    }
+
+    const memos: Map<string, number> = new Map()
+    const dp = (idx: number, prefix: number): number => {
+        if (filteredArr[idx] === undefined) return 0
+
+        if (!memos.has(`${idx},${prefix}`)) {
+            if ((prefix ^ encodedArr[idx]) === (prefix | encodedArr[idx])) {
+                memos.set(`${idx},${prefix}`, Math.max(
+                    filteredArr[idx].length + dp(idx+1, prefix | encodedArr[idx]),
+                    dp(idx + 1, prefix)
+                ))
+            } else {
+                memos.set(`${idx},${prefix}`, dp(idx + 1, prefix))
+            }
+        }
+
+        return memos.get(`${idx},${prefix}`)
+    }
+
+    return dp(0, 0)
+};
