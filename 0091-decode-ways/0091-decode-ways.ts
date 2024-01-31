279fc7d684e3fb381d4023c601200f899b225e70
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-01-31 09:54:02 +0700

    Time: 69 ms (26.24%), Space: 52.2 MB (5.2%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.ts
+++ 0091-decode-ways/0091-decode-ways.ts
@@ -1,28 +1,28 @@
 function numDecodings(s: string): number {
     let res: number = 0;
-    const memorize: {[key in string]?: number} = {};
+    const memorize: Map<string, number> = new Map()
 
     const traveling = function (s: string): boolean {
-        if (memorize[s]) {
-            res += memorize[s];
-            return true;
-        }
         if (s[0] === '0') return false;
         if (s.length === 1) {
-            res+=1;
+            res += 1;
+            return true;
+        }
+
+        if (memorize.has(s)) {
+            res += memorize.get(s)
             return true;
         }
 
         if (traveling(s.substring(1))) {
-            memorize[s] = res;
+            memorize.set(s, res)
         }
-        if (parseInt(s[0]+s[1]) < 27) {
+        if (parseInt(s[0] + s[1]) < 27) {
             if (s.length === 2) {
                 res += 1;
-                memorize[s] = res;
-                return true;
+                memorize.set(s, res)
             } else if (s.length > 2 && traveling(s.substring(2))) {
-                memorize[s] = res;
+                memorize.set(s, res)
             }
         }
         return true;
