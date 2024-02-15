2aec005777cfe3273f6a8857190ecd9f0480ab3f
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-02-15 08:28:37 +0700

    Time: 212 ms (35.29%), Space: 68.9 MB (8.82%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.ts
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.ts
@@ -4,9 +4,9 @@ function largestPerimeter(nums: number[]): number {
     const prefixSum: number[] = [nums[0], nums[0] + nums[1]]
 
     for (let i = 2; i < nums.length; i++) {
-        prefixSum[i] = prefixSum[i-1] + nums[i]
-        if (nums[i] < prefixSum[i-1]) {
-            res = prefixSum[i]
+        [prefixSum[0], prefixSum[1]] = [prefixSum[1], prefixSum[1] + nums[i]]
+        if (nums[i] < prefixSum[0]) {
+            res = prefixSum[1]
         }
     }
 
