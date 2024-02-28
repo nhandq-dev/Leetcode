a4546a6c7bd55c31f374e832d4d3181f2109cea0
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-02-28 23:01:56 +0700

    Time: 80 ms (51.22%), Space: 54.1 MB (46.34%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.ts
@@ -0,0 +1,18 @@
+function maxProfit(k: number, prices: number[]): number {
+    const profit = Array.from(Array(prices.length), () => new Array(2*k+1));
+
+    const dp = (i: number, tno: number, p: number[]) => {
+        if(i == p.length || tno == 0){  
+            return 0; 
+        }
+        if(profit[i][tno] != undefined){ 
+            return profit[ i][tno];
+        }
+        if(tno%2 == 0){
+            return profit[i][tno] = Math.max(-p[i]+dp(i+1, tno-1, p), dp(i+1, tno, p));
+        }
+        return profit[i][tno] = Math.max(p[i]+dp(i+1, tno-1, p), dp(i+1, tno, p));
+    }
+
+    return dp(0, 2*k, prices);
+};
