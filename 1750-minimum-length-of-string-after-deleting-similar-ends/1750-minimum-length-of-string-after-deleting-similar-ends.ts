f8da16525c3bbb7eeab90543405ab8ef3cabbc4c
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-03-05 10:11:52 +0700

    Time: 62 ms (91.67%), Space: 55.4 MB (33.33%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.ts
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.ts
@@ -1,20 +1,17 @@
 function minimumLength(s: string): number {
-    const sAsArr: string[] = s.split('')
     let l = 0
     let r = s.length - 1
 
-    while (l < r && sAsArr[l] === sAsArr[r]) {
-        const commonChar = sAsArr[r]
-        while (sAsArr[l] === commonChar) {
-            sAsArr.shift()
-            r -= 1
+    while (l < r && s[l] === s[r]) {
+        const commonChar = s[r]
+        while (s[l] === commonChar) {
+            l += 1
         }
 
-        while (commonChar === sAsArr[r]) {
-            sAsArr.pop()
+        while (commonChar === s[r]) {
             r -= 1
         }
     }
 
-    return sAsArr.length
+    return r < l ? 0 : r - l + 1
 };
