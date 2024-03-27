41d03e98b757258dcd37f68ad483b03243a1021d
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-03-27 12:31:15 +0700

    Time: 85 ms (43.73%), Space: 72.3 MB (5.83%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.ts
+++ 0041-first-missing-positive/0041-first-missing-positive.ts
@@ -1,14 +1,9 @@
 function firstMissingPositive(nums: number[]): number {
     nums = nums.filter((n) => n>0)
-    const n = nums.length
-    nums = nums.filter((num) => num<=n)
-
     const setOfNum = new Set(nums)
     let candidate = 1
-
     while (setOfNum.has(candidate)) {
         candidate++
     }
-
     return candidate
 };
