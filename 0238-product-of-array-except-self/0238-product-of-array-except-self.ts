6289a47fe3a8f89567cdf4f96835545a29e5a164
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-04-16 16:36:20 +0700

    Time: 1237 ms (5.02%), Space: 67.6 MB (10.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,26 +1,43 @@
 function productExceptSelf(nums: number[]): number[] {
-    let productWithout0: number = 1;
-    let zeroPosition: number = -1;
+    let zeroIdx = -1
+    const prefixProd: number[] = []
 
-    for (let i=0; i<nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 0) {
-            if (zeroPosition !== -1) {
-                return new Array(nums.length).fill(0);
-            }
-            zeroPosition = i;
-        } else {
-            productWithout0 *= nums[i];
+            if (zeroIdx !== -1) return Array(nums.length).fill(0)
+            zeroIdx = i
+            prefixProd[i] = (prefixProd[i-1] || 1)
+            continue
         }
-    }
-    if (zeroPosition !== -1) {
-        const result: number[] = new Array(nums.length).fill(0);
-        result.splice(zeroPosition, 1, productWithout0)
-        return result;
+        prefixProd[i] = (prefixProd[i-1] || 1) * nums[i]
     }
 
-    const res: number[] = [];
-    for (let i=0; i<nums.length; i++) {
-        res.push(productWithout0/nums[i]);
+    if (zeroIdx !== -1) return Array.from({length: nums.length}).map((_, idx) => idx === zeroIdx ? prefixProd[prefixProd.length-1] : 0)
+
+    const suffixProd = [nums[nums.length-1]]
+    for (let i=nums.length-2; i>=0; i--) {
+        suffixProd.unshift(suffixProd[0]*nums[i])
     }
-    return res;
+
+    return Array.from({length: nums.length}).map((_, idx) => {
+        if (idx === 0) {
+            return suffixProd[1]
+        } else if (idx === nums.length - 1) {
+            return prefixProd[suffixProd.length - 2]
+        }
+        
+        return prefixProd[idx-1] * suffixProd[idx+1]
+    })
 };
+
+
+/**
+
+1   2   3   4
+1   2   6   24
+24  24  12  4
+
+24
+1*
+
+ */
