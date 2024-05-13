ed6d48df3b7a6ee1751e5d820dccff91c67c0c11
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-05-13 20:58:18 +0700

    Time: 2435 ms (5.01%), Space: 66.3 MB (28.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,37 +1,18 @@
 function productExceptSelf(nums: number[]): number[] {
-    let zeroIdx = -1
-    const prefixProd: number[] = []
+    const prefixProd: number[] = [nums[0]]
+    const n = nums.length
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            if (zeroIdx !== -1) return Array(nums.length).fill(0)
-            zeroIdx = i
-            prefixProd[i] = (prefixProd[i-1] || 1)
-            continue
-        }
-        prefixProd[i] = (prefixProd[i-1] || 1) * nums[i]
+    for (let i = 1; i < n; i++) {
+        prefixProd[i] = prefixProd[i - 1] * nums[i]
     }
 
-    if (zeroIdx !== -1) return Array.from({length: nums.length}).map((_, idx) => idx === zeroIdx ? prefixProd[prefixProd.length-1] : 0)
-
-    const res = [prefixProd[prefixProd.length - 2]]
-    const suffixProd = [nums[nums.length-1]]
-    for (let i=nums.length-2; i>=0; i--) {
-        suffixProd.unshift(suffixProd[0]*nums[i])
-        res.unshift((prefixProd[i-1] || 1) * suffixProd[1])
+    const res = [prefixProd[n - 2]]
+    const suffixProd = [nums[n - 1]]
+    for (let i = n - 2; i > 0; i--) {
+        suffixProd.unshift(suffixProd[0] * nums[i])
+        res.unshift(prefixProd[i - 1] * suffixProd[1])
     }
+    res.unshift(suffixProd[0])
 
     return res
 };
-
-
-/**
-
-1   2   3   4
-1   2   6   24
-            6
-
-24
-1*
-
- */
