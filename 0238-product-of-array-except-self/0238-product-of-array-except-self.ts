e8a72f82c977865eb1f71d77f5aa98750cf91c79
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-05-13 20:47:25 +0700

    Time: 2576 ms (5.01%), Space: 66.2 MB (28.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -14,20 +14,14 @@ function productExceptSelf(nums: number[]): number[] {
 
     if (zeroIdx !== -1) return Array.from({length: nums.length}).map((_, idx) => idx === zeroIdx ? prefixProd[prefixProd.length-1] : 0)
 
+    const res = [prefixProd[prefixProd.length - 2]]
     const suffixProd = [nums[nums.length-1]]
     for (let i=nums.length-2; i>=0; i--) {
         suffixProd.unshift(suffixProd[0]*nums[i])
+        res.unshift((prefixProd[i-1] || 1) * suffixProd[1])
     }
 
-    return Array.from({length: nums.length}).map((_, idx) => {
-        if (idx === 0) {
-            return suffixProd[1]
-        } else if (idx === nums.length - 1) {
-            return prefixProd[suffixProd.length - 2]
-        }
-        
-        return prefixProd[idx-1] * suffixProd[idx+1]
-    })
+    return res
 };
 
 
@@ -35,7 +29,7 @@ function productExceptSelf(nums: number[]): number[] {
 
 1   2   3   4
 1   2   6   24
-24  24  12  4
+            6
 
 24
 1*
