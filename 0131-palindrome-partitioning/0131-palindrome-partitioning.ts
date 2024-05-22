65c66bbeb063ce2854230e7d5a70fa686377f94c
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-05-22 22:13:46 +0700

    Time: 233 ms (45.45%), Space: 85.3 MB (5.14%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.ts
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.ts
@@ -1,67 +1,12 @@
-class PalindromeString {
-    private firstHaft: string;
-    private secondHaft: string;
-
-    constructor() {
-        this.firstHaft = ''
-        this.secondHaft = ''
-    }
-
-    isPalindrome(): boolean {
-        if (this.firstHaft === '' && this.secondHaft === '') {
-            return false
-        }
-        const minLength = Math.min(this.firstHaft.length, this.secondHaft.length)
-
-        return this.firstHaft.length === this.secondHaft.length ?
-            this.firstHaft === this.secondHaft :
-            this.firstHaft.length === minLength ?
-                this.firstHaft === this.secondHaft.slice(0, minLength) :
-                this.firstHaft.slice(0, minLength) === this.secondHaft
-    }
-
-    insert(char: string): void {
-        const minLength = Math.min(this.firstHaft.length, this.secondHaft.length)
-
-        if (this.firstHaft.length !== this.secondHaft.length && this.firstHaft.length === minLength) {
-            this.firstHaft = `${this.firstHaft}${this.secondHaft[minLength]}`
-            this.secondHaft = this.secondHaft.substring(0, minLength)
-        }
-
-        this.secondHaft = `${char}${this.secondHaft}`
-    }
-
-    pop(): void {
-        this.secondHaft = this.secondHaft.substring(0, this.secondHaft.length - 1)
-        const minLength = Math.min(this.firstHaft.length, this.secondHaft.length)
-
-        if (this.firstHaft.length !== this.secondHaft.length && this.secondHaft.length === minLength) {
-            this.secondHaft = `${this.secondHaft}${this.firstHaft[minLength]}`
-            this.firstHaft = this.firstHaft.substring(0, minLength)
-        }
-    }
-
-    get(): string {
-        return `${this.firstHaft}${this.secondHaft.split('').reverse().join('')}`
-    }
-}
-
 function partition(s: string): string[][] {
-    const setOfPalindrome: Map<string, boolean> = new Map()
-    const isPalindrome = (str: string): boolean => {
+    const isPalindrome = _.memoize((str: string): boolean => {
         if (str.length === 0) return false
+        const mid = Math.floor(str.length / 2)
 
-        if (!setOfPalindrome.has(str)) {
-            const mid = Math.floor(str.length / 2)
-
-            setOfPalindrome.set(str, str.length % 2 === 0 ?
-                str.substring(0, mid) === str.substring(mid).split('').reverse().join('') :
-                str.substring(0, mid) === str.substring(mid + 1).split('').reverse().join('')
-            )
-        }
-
-        return setOfPalindrome.get(str)
-    }
+        return str.length % 2 === 0 ?
+            str.substring(0, mid) === str.substring(mid).split('').reverse().join('') :
+            str.substring(0, mid) === str.substring(mid + 1).split('').reverse().join('')
+    })
 
     const backTracking = _.memoize((idx: number): string[][] => {
         if (s[idx] === undefined) return []
