16f189b2eeae208d69c4a4d55f8cda084de8c8ae
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-05-25 20:49:45 +0700

    Time: 2210 ms (5.28%), Space: 84.8 MB (5.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.ts
+++ 0322-coin-change/0322-coin-change.ts
@@ -1,20 +1,15 @@
 function coinChange(coins: number[], amount: number): number {
-    const memos: Map<string, number> = new Map()
-
-    const dp = (idx: number, target: number) => {
+    const dp = _.memoize((idx: number, target: number) => {
         if (target === 0) return 0
         if (coins[idx] === undefined) return Number.POSITIVE_INFINITY
         if (target < coins[idx]) return dp(idx + 1, target)
 
-        if (!memos.has(`${idx}-${target}`)) {
-            memos.set(`${idx}-${target}`, Math.min(
-                dp(idx + 1, target),
-                1 + dp(idx, target - coins[idx]),
-            ))
-        }
+        return Math.min(
+            dp(idx + 1, target),
+            1 + dp(idx, target - coins[idx]),
+        )
+    }, (arg1, arg2) => `${arg1}-${arg2}`)
+    const res = dp(0, amount)
 
-        return memos.get(`${idx}-${target}`)
-    }
-
-    return dp(0, amount) === Number.POSITIVE_INFINITY ? -1 : dp(0, amount)
+    return res === Number.POSITIVE_INFINITY ? -1 : res
 };
