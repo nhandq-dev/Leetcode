e8c201c62c046657629c7a3a08c5b8bce86de25c
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-05-28 10:29:30 +0700

    Time: 108 ms (7.41%), Space: 59.4 MB (7.41%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.ts
@@ -0,0 +1,29 @@
+function equalSubstring(s: string, t: string, maxCost: number): number {
+    const getChangeCost = _.memoize((char1: string, char2: string): number => Math.abs(char1.charCodeAt(0) - char2.charCodeAt(0)), (arg1: string, arg2: string) => `${arg1}-${arg2}`)
+    const n = s.length
+    let res = 0
+    let l = 0, r = 0
+    let candidate = 0
+
+    while (l<n && r<n) {
+        const changeCost = getChangeCost(s[r], t[r])
+        while (changeCost > maxCost && r > l) {
+            maxCost += getChangeCost(s[l], t[l])
+            l++
+            candidate--
+        }
+        if (changeCost > maxCost) {
+            l++
+            r++
+            candidate = 0
+            continue
+        }
+
+        maxCost -= changeCost
+        r++
+        candidate++
+        res = Math.max(res, candidate)
+    }
+
+    return res
+};
