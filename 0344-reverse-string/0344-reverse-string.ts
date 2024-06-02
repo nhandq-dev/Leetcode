2d1c1a01b1ff269c018fddb13a7a8dc60c745476
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-06-02 08:09:58 +0700

    Time: 91 ms (18.58%), Space: 58.4 MB (16.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -2,9 +2,8 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    for (let i: number = 0; i<=Math.floor(s.length/2); i++) {
-        s.splice(s.length - i, 0, s[i])
-        s[i] = s[s.length - 2 - i];
-        s.splice(s.length - 2 - i, 1);
+    const n = s.length
+    for (let i: number = 0; i < Math.floor(n / 2); i++) {
+        [s[i], s[n-1-i]] = [s[n-1-i], s[i]]
     }
 };
