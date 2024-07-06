70f6d3dbdbdd5e466f2c1bd919e84817d9647399
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-07-06 10:52:41 +0700

    Time: 56 ms (56.25%), Space: 49.7 MB (75%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.ts
+++ 2582-pass-the-pillow/2582-pass-the-pillow.ts
@@ -1,5 +1,8 @@
 function passThePillow(n: number, time: number): number {
     n -= 1
-    const div = Math.floor(time / n)
-    return 1 + (time <= n ? time : div % 2 === 0 ? time % n : n - time % n)
+    return 1 + (
+            Math.floor(time / n) % 2 === 0 ?
+                time % n :
+                n - time % n
+    )
 };
