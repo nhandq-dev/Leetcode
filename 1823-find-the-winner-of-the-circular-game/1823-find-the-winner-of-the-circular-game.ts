9eb667367733b1b16811d4ae1f5e22df2c5cac7b
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-07-08 11:19:31 +0700

    Time: 60 ms (62.16%), Space: 51.9 MB (48.65%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.ts
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.ts
@@ -1,14 +1,13 @@
 function findTheWinner(n: number, k: number): number {
-    const friends: number[] = Array.from({ length: n }, (_, idx: number) => idx + 1)
+    const nums: number[] = Array.from({ length: n }, (_, idx: number) => idx + 1)
+    let currIdx = 0
 
-    const travelling = (nums: number[], currIdx: number): number => {
-        if (nums.length === 1) return nums[0]
-
+    while (nums.length > 1) {
         currIdx = (currIdx + k) % nums.length - 1
         currIdx = currIdx === -1 ? nums.length - 1 : currIdx
+
         nums.splice(currIdx, 1)
-        return travelling(nums, currIdx % nums.length)
     }
 
-    return travelling(friends, 0)
+    return nums[0]
 };
