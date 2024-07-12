facb45715b82489cbff975c67cf2c8f5a46de566
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-07-12 15:35:56 +0700

    Time: 714 ms (100%), Space: 61.5 MB (100%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.ts
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.ts
@@ -1,23 +1,73 @@
-function maximumGain(s: string, x: number, y: number): number {
-    let n = s.length
-    const biggerPriceMask = x > y ? /ab/gi : /ba/gi
-    const smallerPriceMask = x > y ? /ba/gi : /ab/gi
-    let res = 0
+function minimize(s: string): string {
+    let res = []
 
-    let filtered = s.replaceAll(biggerPriceMask, '')
-    while (filtered.length < s.length) {
-        s = filtered
-        filtered = s.replaceAll(biggerPriceMask, '')
-    }
-    res += x > y ? x * ((n - filtered.length) / 2) : y * ((n - filtered.length) / 2)
-    n = filtered.length
+    for (let i = 0; i < s.length - 1; i++) {
+        let checker = false
+        let subStr = s.at(i)
 
-    filtered = s.replaceAll(smallerPriceMask, '')
-    while (filtered.length < s.length) {
-        s = filtered
-        filtered = s.replaceAll(smallerPriceMask, '')
+        if (s.at(i) === 'a') {
+            for (let j = i + 1; j < s.length; j++, i++) {
+                if (s.at(j) === 'b') checker = true
+                if (s.charCodeAt(j) > 98) break
+                subStr += s.at(j)
+            }
+
+            if (checker) {
+                res.push(subStr)
+            }
+        } else if (s.at(i) === 'b') {
+            for (let j = i + 1; j < s.length; j++, i++) {
+                if (s.at(j) === 'a') checker = true
+                if (s.charCodeAt(j) > 98) break
+                subStr += s.at(j)
+            }
+
+            if (checker) {
+                res.push(subStr)
+            }
+        }
     }
-    res += x > y ? y * ((n - filtered.length) / 2) : x * ((n - filtered.length) / 2)
+
+    return res.join('o')
+}
+
+function removingSubFromS(s: string, match: string) {
+    const w1 = match.at(0), w2 = match.at(1)
+    let res = ''
+    let stackCount = 0
+
+    for (let i = 0; i < s.length; i++) {
+        res += s.at(i)
+
+        if (s.at(i) === w1) {
+            stackCount++
+        } else if (s.at(i) === w2 && stackCount > 0) {
+            stackCount--
+            res = res.substring(0, res.length - 2)
+        } else {
+            stackCount = 0
+        }
+    }
 
     return res
+}
+
+function maximumGain(s: string, x: number, y: number): number {
+    // s = minimize(s)
+    let n = s.length
+    let res = 0
+
+    if (x > y) {
+        s = removingSubFromS(s, 'ab')
+        res = ((n - s.length) / 2) * x
+        n = s.length
+        s = removingSubFromS(s, 'ba')
+        return res + ((n - s.length) / 2) * y
+    }
+
+    s = removingSubFromS(s, 'ba')
+    res = ((n - s.length) / 2) * y
+    n = s.length
+    s = removingSubFromS(s, 'ab')
+    return res + ((n - s.length) / 2) * x
 };
