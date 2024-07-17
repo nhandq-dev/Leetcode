562595e41078a0925ea67463b8f6811aa79db987
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-07-17 10:03:55 +0700

    Time: 83 ms (75.93%), Space: 58.2 MB (85.19%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function delNodes(root: TreeNode | null, to_delete: number[]): Array<TreeNode | null> {
+    const res: (TreeNode | null)[] = []
+    const toDelete: Set<number> = new Set(to_delete)
+
+    const travelling = (node: TreeNode | null): TreeNode | null => {
+        if (node === null) return null
+
+        if (toDelete.has(node.val)) {
+            toDelete.delete(node.val)
+            const leftTravel = travelling(node.left)
+            const rightTravel = travelling(node.right)
+            if (leftTravel) res.push(travelling(node.left))
+            if (rightTravel) res.push(travelling(node.right))
+
+            return null
+        }
+
+        node.left = travelling(node.left)
+        node.right = travelling(node.right)
+        return node
+    }
+    const mainNode = travelling(root)
+    if (mainNode) res.push(travelling(root))
+
+    return res
+};
