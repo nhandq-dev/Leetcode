27195300aa360342578afafd85b473a5d941e71a
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2024-07-18 11:27:32 +0700

    Time: 352 ms (9.09%), Space: 63.5 MB (18.18%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.ts
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countPairs(root: TreeNode | null, distance: number): number {
+    let res = 0
+    const leafNode: string[] = []
+
+    const travelling = (node: TreeNode | null, path = '') => {
+        if (node.left === null && node.right === null) leafNode.push(path)
+
+        if (node.left !== null) {
+            travelling(node.left, `${path}L`)
+        }
+        if (node.right !== null) {
+            travelling(node.right, `${path}R`)
+        }
+    }
+    travelling(root, 'U')
+
+    for (let i = 0; i < leafNode.length - 1; i++) {
+        for (let j = i + 1; j < leafNode.length; j++) {
+            let leaf1 = leafNode[i]
+            let leaf2 = leafNode[j]
+
+            while (leaf1.at(0) === leaf2.at(0)) {
+                leaf1 = leaf1.slice(1)
+                leaf2 = leaf2.slice(1)
+            }
+
+            res += leaf1.length + leaf2.length <= distance ? 1 : 0
+        }
+    }
+
+    return res
+};
+
+/**
+98 1 1
+15 2 2
+78 3 2
+
+
+ */
