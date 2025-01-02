2bc9430cd6daeae6946195dc9c9131a3f75149d8
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-01-02 07:24:05 +0700

    Time: 31 ms (9.09%), Space: 81.3 MB (9.09%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.ts
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.ts
@@ -1,5 +1,19 @@
 function vowelStrings(words: string[], queries: number[][]): number[] {
     const VOWEL_LETTERS = ['a', 'e', 'o', 'u', 'i']
+    const prefixSum: number[] = [0]
+    const validString = _.memoize((str: string) => {
+        return VOWEL_LETTERS.includes(str.charAt(0)) && VOWEL_LETTERS.includes(str.charAt(str.length - 1))
+    })
+    for (const w of words) {
+        prefixSum.push(prefixSum[prefixSum.length - 1] + validString(w))
+    }
+
+    return queries.map(([s, e]) => prefixSum[e + 1] - prefixSum[s])
+};
+
+/**
+function vowelStrings(words: string[], queries: number[][]): number[] {
+    const VOWEL_LETTERS = ['a', 'e', 'o', 'u', 'i']
     const validString = _.memoize((str: string) => {
         return VOWEL_LETTERS.includes(str.charAt(0)) && VOWEL_LETTERS.includes(str.charAt(str.length - 1))
     })
@@ -12,3 +26,4 @@ function vowelStrings(words: string[], queries: number[][]): number[] {
         return res
     })
 };
+ */
