ad507927b13b631372db503b56a338cc2756f31d
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-01-03 08:17:41 +0700

    Time: 11 ms (80.39%), Space: 64.1 MB (80.39%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.ts
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.ts
@@ -1,14 +1,16 @@
 function waysToSplitArray(nums: number[]): number {
-    const total = nums.reduce((carry: number, num: number) => carry + num, 0)
-    let currSum = 0
-    let res = 0
+    const totalSum = nums.reduce((carry: number, num: number) => carry + num, 0);
+    let leftSum = 0;
+    let result = 0;
 
     for (let i = 0; i < nums.length - 1; i++) {
-        currSum += nums[i]
-        if (currSum >= total - currSum) {
-            res += 1
+        leftSum += nums[i];
+        const rightSum = totalSum - leftSum;
+
+        if (leftSum >= rightSum) {
+            result++;
         }
     }
 
-    return res
+    return result;
 };
