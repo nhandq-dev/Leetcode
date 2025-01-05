063910a7bbb7e1b4e541eca01b8ffbbeb7a12881
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-01-05 21:12:02 +0700

    Time: 59 ms (40%), Space: 73.5 MB (100%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.ts
@@ -0,0 +1,15 @@
+function shiftingLetters(s: string, shifts: number[][]): string {
+    const n = s.length
+    const diffArr: number[] = Array.from({ length: n + 1 }, _ => 26)
+
+    for (const [start, end, direction] of shifts) {
+        diffArr[start] = (diffArr[start] + direction * 2 - 1) % 52  + 52
+        diffArr[end + 1] = (diffArr[end + 1] - direction * 2 + 1) % 52 + 52
+    }
+
+    let carry = 0
+    return s.split('').map((_, idx: number) => {
+        carry += diffArr[idx]
+        return String.fromCharCode(97 + (s.charCodeAt(idx) + carry - 26 - 97) % 26)
+    }).join('')
+};
