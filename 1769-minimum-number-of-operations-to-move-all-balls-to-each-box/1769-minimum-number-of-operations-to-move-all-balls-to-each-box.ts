e0d6d87e16baf3583243789bd93a2f882fe679bf
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-01-07 07:04:30 +0700

    Time: 15 ms (52%), Space: 55.9 MB (8%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.ts
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.ts
@@ -1,18 +1,19 @@
 function minOperations(boxes: string): number[] {
-    const n = boxes.length;
-    const ans = new Array(n).fill(0);
-    for (let i = 1, count = 0; i < n; i++) {
-        if (boxes[i - 1] === '1') {
-            count++;
-        }
-        ans[i] = ans[i - 1] + count;
+    const n = boxes.length
+    const prefixStep: number[] = [+boxes.charAt(0)]
+    const sufixStep: number[] = [+boxes.charAt(n - 1)]
+
+    for (let i = 0; i < n - 1; i++) {
+        prefixStep.push(prefixStep[i] + +boxes.charAt(i + 1))
+        sufixStep.push(sufixStep[i] + +boxes.charAt(n - i - 2))
     }
-    for (let i = n - 2, count = 0, sum = 0; i >= 0; i--) {
-        if (boxes[i + 1] === '1') {
-            count++;
-        }
-        sum += count;
-        ans[i] += sum;
+
+    for (let i = 1; i < n; i++) {
+        prefixStep[i] += prefixStep[i - 1]
+        sufixStep[i] += sufixStep[i - 1]
     }
-    return ans;
-}
+
+    return Array.from({ length: n }).map((_, idx: number) => {
+        return (sufixStep[n - 2 - idx] || 0) + (prefixStep[idx-1] || 0)
+    })
+};
