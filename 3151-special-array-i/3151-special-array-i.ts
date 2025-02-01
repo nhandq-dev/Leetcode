88c11a72c8e35ae3bbdf79960262db50730c8fea
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-02-01 10:07:14 +0700

    Time: 0 ms (100%), Space: 51.3 MB (100%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.ts
@@ -0,0 +1,6 @@
+function isArraySpecial(nums: number[]): boolean {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if ((nums[i] % 2 + nums[i + 1] % 2) % 2 === 0) return false
+    }
+    return true
+};
