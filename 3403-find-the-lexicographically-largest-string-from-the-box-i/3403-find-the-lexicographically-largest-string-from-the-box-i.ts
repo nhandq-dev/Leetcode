85bc23f14252686b3979c216dd921a384440ba6d
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-06-04 18:31:04 +0700

    Time: 7 ms (15.38%), Space: 59.3 MB (15.38%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.ts
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.ts
@@ -3,8 +3,8 @@ function answerString(word: string, numFriends: number): string {
     let answer = ''
 
     for (let i = 0; i < word.length; i++) {
-        const candidate = word.slice(i, Math.min(word.length, i + word.length - numFriends + 1))
-        if (candidate > answer) answer = candidate
+        if (word.charAt(i) >= answer.charAt(0) && word.slice(i, Math.min(word.length, i + word.length - numFriends + 1)) > answer)
+            answer = word.slice(i, Math.min(word.length, i + word.length - numFriends + 1))
     }
 
     return answer
