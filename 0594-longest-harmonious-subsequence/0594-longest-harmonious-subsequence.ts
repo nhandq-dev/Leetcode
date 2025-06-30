9600086237177063d17dfaa0e4238a9cc901e68a
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-06-30 09:32:52 +0700

    Time: 21 ms (59.18%), Space: 65.5 MB (59.86%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
@@ -1,20 +1,21 @@
 function findLHS(nums: number[]): number {
     const numFreq: Map<number, number> = new Map()
+    let answer = 0
 
     for (const num of nums) {
         numFreq.set(num, (numFreq.get(num) || 0) + 1)
     }
-    const numFreqArr: number[][] = Array.from(numFreq.entries())
 
-    let answer = 0
-    for (let i = 0; i < numFreqArr.length; i++) {
-        const [num, freq] = numFreqArr[i]
-
-        if (numFreq.has(num - 1))
-            answer = Math.max(answer, numFreqArr[i][1] + numFreq.get(num - 1))
-        if (numFreq.has(1 + num))
-            answer = Math.max(answer, numFreqArr[i][1] + numFreq.get(num + 1))
-    }
+    numFreq.forEach((freq, num) => {
+        if (numFreq.has(num - 1)) {
+            answer = Math.max(answer, freq + numFreq.get(num - 1))
+            numFreq.delete(num)
+        }
+        if (numFreq.has(num + 1)) {
+            answer = Math.max(answer, freq + numFreq.get(num + 1))
+            numFreq.delete(num)
+        }
+    })
 
     return answer
 };
