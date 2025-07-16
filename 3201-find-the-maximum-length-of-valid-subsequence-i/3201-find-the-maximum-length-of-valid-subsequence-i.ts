dfbce9e986ac9fd45602371e37db3633b4226883
Nhandq <56851883+nhandq-dev@users.noreply.github.com>
2025-07-16 09:28:57 +0700

    Time: 7 ms (75%), Space: 79 MB (25%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.ts
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.ts
@@ -1,24 +1,23 @@
 function maximumLength(nums: number[]): number {
-    const modularNums = nums.map(n => n % 2)
     const candidateStartAtZero = []
     const candidateStartAtOne = []
     let countOfZero = 0
 
-    for (const num of modularNums) {
-        if (num === 0) {
+    for (const num of nums) {
+        if (num % 2 === 0) {
             if (candidateStartAtZero.length === 0 || candidateStartAtZero[candidateStartAtZero.length - 1] === 1) {
-                candidateStartAtZero.push(num)
+                candidateStartAtZero.push(num % 2)
             }
             if (candidateStartAtOne[candidateStartAtOne.length - 1] === 1) {
-                candidateStartAtOne.push(num)
+                candidateStartAtOne.push(num % 2)
             }
             countOfZero++
         } else {
             if (candidateStartAtOne.length === 0 || candidateStartAtOne[candidateStartAtOne.length - 1] === 0) {
-                candidateStartAtOne.push(num)
+                candidateStartAtOne.push(num % 2)
             }
             if (candidateStartAtZero[candidateStartAtZero.length - 1] === 0) {
-                candidateStartAtZero.push(num)
+                candidateStartAtZero.push(num % 2)
             }
         }
     }
